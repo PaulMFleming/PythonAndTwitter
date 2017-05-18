19e8ec52bb0290914b19d93a7d17e1ce413773ed
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-05-18 22:38:45 +0100

    Added retweet_popularity.py

--- retweet_popularity.py
+++ retweet_popularity.py
@@ -23,3 +23,29 @@ query = 'Ireland'
 # Get all tweets for the search query
 results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
 
+# controls how many retweets a tweet 
+# needs in order to be considered popular
+min_retweets = 10 
+
+# loops through results to determine if a tweet is popular
+# if it is it's added to the pop_tweets list
+pop_tweets = [ status
+            for staus in results
+            if status._json['retweet_count'] > min_retweets ]
+
+# create a list of tweet tuples associating each 
+# tweet's text with it's retweet count
+tweet_tups = [(tweet._json['text'].encode('utf-8'), 
+                tweet._json['retweet_count'])
+                for tweet in pop_tweets]
+
+# sort the tuple entries in descending order
+most_popular_tups = sorted(tweet_tups, key=itemgetter(1), reverse=True)[:5]
+
+# prettify 
+table = PrettyTable(field_names=['Text', 'Retweet Count'])
+for key, val in most_popular_tups:
+    table.add_row([key, val])
+table.max_width['Text'] = 50
+table.align['Text'], table.align['Retweet Count'] = 'l', 'r' # align colums
+print table
