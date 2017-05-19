3e6f6e026f09b7445e17c71d7de1036f4a151af8
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-05-19 18:59:37 +0100

    added users_timeline_tweets.py

---
+++ users_timeline_tweets.py
@@ -0,0 +1,17 @@
+import tweepy
+from tweepy import OAuthHandler
+from settings import twitter_app_config
+
+CONSUMER_KEY = twitter_app_config.get('CONSUMER_KEY')
+CONSUMER_SECRET = twitter_app_config.get('CONSUMER_SECRET')
+OAUTH_TOKEN = twitter_app_config.get('OAUTH_TOKEN')
+OAUTH_TOKEN_SECRET = twitter_app_config.get('OAUTH_TOKEN_SECRET')
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+for status in tweepy.Cursor(api.home_timeline).items(10):
+    # Process a tweet
+    print status.text
